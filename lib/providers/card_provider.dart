8be8b9d766733271afd38307018b703c116c57d6
Oliver Hermens <hermenoliv@ecs.vuw.ac.nz>
2022-10-06 00:20:56 +1300

    Card provider updating storage

--- lib/providers/card_provider.dart
+++ lib/providers/card_provider.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:swen325_assignment_3/data/business_card.dart';
+import 'package:swen325_assignment_3/services/secure_storage.dart';
 
 class Cards with ChangeNotifier {
+  final SecureStorage storage = SecureStorage();
   List<BusinessCard> _cards = [];
 
   List<BusinessCard> get cards => _cards;
 
   void add(BusinessCard b) {
     _cards.add(b);
+    storage.writeCard(b);
     notifyListeners();
   }
 
   void delete(BusinessCard b) {
     _cards.remove(b);
+    storage.deleteCard(b.id);
     notifyListeners();
   }
 
   void deleteAll(List<BusinessCard> del) {
     del.forEach((element) {
+      storage.deleteCard(element.id);
       _cards.remove(element);
     });
     notifyListeners();
@@ -25,6 +30,12 @@ class Cards with ChangeNotifier {
 
   void clear() {
     _cards.clear();
+    storage.clear();
+    notifyListeners();
+  }
+
+  void loadFromStorage() async {
+    _cards = await storage.readCards();
     notifyListeners();
   }
 
