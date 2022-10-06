c7310d745d75ee7bb680e6c4848a6183e5ddb259
Nicholas Woolf-Ben-Avraham <woolfnich@ecs.vuw.ac.nz>
2022-10-06 17:36:47 +1300

    Ensure card list has cards if it is to be used.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
+import 'package:swen325_assignment_3/providers/card_provider.dart';
 import '../main.dart';
 import 'scan.dart';
 import 'userCards.dart';
@@ -27,7 +30,15 @@ class _HomeState extends State<Home> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Carousel(),
+              context.read<Cards>().isEmpty(false)
+                  ? Column(children: const [
+                      Text(
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 24),
+                          "No Cards"),
+                      SizedBox(height: 125)
+                    ])
+                  : Carousel(),
               LogoButton(
                 text: 'Scan',
                 onClicked: () => Navigator.push(
@@ -42,14 +53,31 @@ class _HomeState extends State<Home> {
                   context,
                   MaterialPageRoute(builder: (context) => const UserCards()),
                 ),
-                icon: Icon(Icons.library_add, size: 40),
+                icon: const Icon(Icons.library_add, size: 40),
               ),
               LogoButton(
                 text: 'View Wallet',
-                onClicked: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Wallet()),
-                ),
+                onClicked: () => {
+                  if (context.read<Cards>().isEmpty(false))
+                    {
+                      Fluttertoast.showToast(
+                          msg:
+                              "No saved cards. Scan a card to add it to your list!",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.blue,
+                          textColor: Colors.white,
+                          fontSize: 16.0)
+                    }
+                  else
+                    {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Wallet()),
+                      )
+                    }
+                },
                 icon: const Icon(Icons.wallet, size: 40),
               ),
             ],
