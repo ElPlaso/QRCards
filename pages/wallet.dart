7437f0095404e41779673bb549c5c8c3d8da21d9
ElPlaso <66361666+ElPlaso@users.noreply.github.com>
2022-11-02 20:58:06 +1300

    Add files via upload

---
+++ pages/wallet.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:swen325_assignment_3/widgets/floater.dart';
+import '../providers/query_provider.dart';
+import '../widgets/wallet_wheel.dart';
+
+// * Page to display whallet wheel of user's collected cards
+
+class Wallet extends StatelessWidget {
+  const Wallet({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Wallet'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const WalletWheel(),
+
+              /// * This button sends a refresh request to the Database for the
+              /// * most modern version of the QRCards stored in the users wallet.
+              Floater(
+                text: 'Refresh Wallet',
+                onClicked: () async =>
+                    {await context.read<QueryProvider>().updateWallet(context)},
+                icon: const Icon(Icons.refresh, size: 25),
+              ),
+            ],
+          ),
+        ),
+      );
+}
